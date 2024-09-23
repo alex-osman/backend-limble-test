cc407275cbb3b3ff827261e2bf6d5f3f7b1c03c2
Alex Osman <alexosman@Alexs-MBP.fios-router.home>
2024-09-23 17:56:38 -0400

    cleanup enums and add filtering

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -10,23 +10,18 @@ export enum TaskStatus {
   BOTH = "BOTH",
 }
 
-export const costByWorkerRoute = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  const result = await costByWorker(TaskStatus.BOTH);
-  res.send(result);
-};
+export enum APIResponseStatus {
+  SUCCESS = "success",
+  ERROR = "error",
+}
 
-export const costByWorker = async (
-  taskStatus: TaskStatus,
-  workerIds?: number[]
-): Promise<{
-  status: "success" | "error";
+export interface APIResponse {
+  status: APIResponseStatus;
   data: {
     totalCost: number;
     breakdown: {
       workerId: number;
+      workerName: string;
       totalCost: number;
     }[];
   };
@@ -34,36 +29,108 @@ export const costByWorker = async (
     taskStatus: TaskStatus;
     workerIds?: number[];
   };
-}> => {
-  const loggedTimes = await AppDataSource.getRepository(LoggedTime).find({
-    relations: ["worker", "task"],
-  });
+}
 
-  const breakdownDict: { [workerId: number]: number } = loggedTimes.reduce(
-    (workerDict, loggedTime) => {
-      const loggedTimeCost =
-        (loggedTime.time_seconds /
-        SECONDS_IN_HOUR) *
-        loggedTime.worker.hourly_wage;
+export const validateTaskStatus = (taskStatus?: string): TaskStatus => {
+  if (!taskStatus) {
+    throw new Error("The 'taskStatus' query parameter is required.");
+  }
 
-      if (workerDict[loggedTime.worker.id]) {
-        workerDict[loggedTime.worker.id] += loggedTimeCost;
-      } else {
-        workerDict[loggedTime.worker.id] = loggedTimeCost;
-      }
-      return workerDict;
-    },
-    {} as { [workerId: number]: number }
-  );
+  const uppercaseStatus = taskStatus.toUpperCase();
 
-  const breakdown = Object.entries(breakdownDict).map(([workerId, totalCost]) => ({
+  // Check if valid enum
+  if (!(uppercaseStatus in TaskStatus)) {
+    throw new Error(
+      "The 'taskStatus' query parameter must be one of 'COMPLETE', 'INCOMPLETE', or 'BOTH'."
+    );
+  }
+
+  return uppercaseStatus as TaskStatus;
+};
+
+export const validateWorkerIds = (workerIds: any): number[] => {
+  console.log("what is your workerid");
+  if (!workerIds) {
+    return [];
+  }
+  const parsedWorkerIds = workerIds.split(",").map((id) => parseInt(id));
+  if (parsedWorkerIds.some((id) => isNaN(id))) {
+    throw new Error(
+      "workerIds query parameter must be a comma-separated list of integers"
+    );
+  }
+  return parsedWorkerIds;
+};
+
+export const costByWorkerRoute = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  // Get the task status from the query string
+  try {
+    const taskStatus = validateTaskStatus(req.query.taskStatus?.toString());
+    const workerIds =
+      req.query.workerIds && validateWorkerIds(req.query.workerIds);
+
+    const result = await costByWorker(taskStatus);
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e.message);
+  }
+};
+
+export const costByWorker = async (
+  taskStatus: TaskStatus,
+  workerIds?: number[]
+): Promise<APIResponse> => {
+  // Query for logged times
+  const loggedTimesQB = AppDataSource.getRepository(LoggedTime)
+    .createQueryBuilder("logged_time")
+    .leftJoinAndSelect("logged_time.worker", "worker")
+    .leftJoinAndSelect("logged_time.task", "task");
+
+  if (taskStatus !== TaskStatus.BOTH) {
+    loggedTimesQB.where("task.is_complete = :isComplete", {
+      isComplete: taskStatus === TaskStatus.COMPLETE ? true : false,
+    });
+  }
+
+  if (workerIds && workerIds.length > 0) {
+    loggedTimesQB.andWhere("worker.id IN (:...workerIds)", {
+      workerIds,
+    });
+  }
+
+  const loggedTimes = await loggedTimesQB.getMany();
+
+  const breakdownDict: {
+    [workerId: number]: { totalCost: number; name: string };
+  } = loggedTimes.reduce((workerDict, loggedTime) => {
+    const loggedTimeCost =
+      (loggedTime.time_seconds / SECONDS_IN_HOUR) *
+      loggedTime.worker.hourly_wage;
+
+    if (workerDict[loggedTime.worker.id]) {
+      workerDict[loggedTime.worker.id].totalCost += loggedTimeCost;
+    } else {
+      workerDict[loggedTime.worker.id] = {
+        totalCost: loggedTimeCost,
+        name: loggedTime.worker.username,
+      };
+    }
+    return workerDict;
+  }, {} as { [workerId: number]: { totalCost: number; name: string } });
+
+  const breakdown = Object.entries(breakdownDict).map(([workerId, worker]) => ({
     workerId: parseInt(workerId),
-    totalCost,
+    totalCost: worker.totalCost,
+    workerName: worker.name,
   }));
 
   try {
     return {
-      status: "success",
+      status: APIResponseStatus.SUCCESS,
       data: {
         totalCost: breakdown.reduce((acc, { totalCost }) => acc + totalCost, 0),
         breakdown,
@@ -75,7 +142,7 @@ export const costByWorker = async (
     };
   } catch (error) {
     return {
-      status: "error",
+      status: APIResponseStatus.ERROR,
       data: {
         totalCost: 0,
         breakdown: [],
--- server/tests/analytics.controller.test.ts
+++ server/tests/analytics.controller.test.ts
@@ -1,19 +1,16 @@
+import { setupServer } from "../app";
 import { costByWorker, TaskStatus } from "../controllers/analytics.controller";
-import AppDataSource from "../db";
-import { Worker } from "../entities/worker.entity";
+
+beforeAll(async () => {
+  await setupServer();
+});
 
 describe("costByWorker Function", () => {
-  // it("should return correct data for all workers if no workerIds specified", async () => {
-  //   // Mocking the result of this function, replacing with your own test logic later
-  //   const result = await costByWorker(TaskStatus.BOTH);
-  //   console.log("result - ", result);
+  it("should return correct data for all workers if no workerIds specified", async () => {
+    const result = await costByWorker(TaskStatus.BOTH);
+    console.log("result - ", result);
 
-  //   expect(result.status).toBe("success");
-  // });
-
-  it("should access the database just fine", async () => {
-    const workers = await AppDataSource.getRepository(Worker).find();
-    console.log("workers - ", workers);
-    expect(workers.length).toBeGreaterThan(0);
-  })
+    expect(result.status).toBe("success");
+    expect(result.data.breakdown.length).toBe(3);
+  });
 });
