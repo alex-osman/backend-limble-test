8d938b8632f533b3a4f3f6bafdcbcb4e7f7605b0
Alex Osman <alexosman@Alexs-MBP.fios-router.home>
2024-09-23 14:04:41 -0400

    move app to separate file

---
+++ server/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import * as mariadb from "mariadb";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+
+let db: mariadb.Pool;
+
+async function connect() {
+  console.info("Connecting to DB...");
+  db = mariadb.createPool({
+    host: process.env["DATABASE_HOST"],
+    user: process.env["DATABASE_USER"],
+    password: process.env["DATABASE_PASSWORD"],
+    database: process.env["DATABASE_NAME"],
+  });
+
+  const conn = await db.getConnection();
+  try {
+    await conn.query("SELECT 1");
+    console.log("Connection successful...");
+  } finally {
+    await conn.end();
+  }
+}
+
+async function setupServer() {
+  await connect();
+
+  app.get("/", (req, res) => {
+    console.log("Hello");
+    res.send("Hello!");
+  });
+
+  return app
+}
+
+export default setupServer;
--- server/index.ts
+++ server/index.ts
@@ -1,42 +1,12 @@
-import express from "express";
-import * as mariadb from "mariadb";
-import morgan from "morgan";
+import setupServer from "./app.js";
 
-const app = express();
-app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 const port = 3000;
-let db;
-
-async function connect() {
-  console.info("Connecting to DB...");
-  db = mariadb.createPool({
-    host: process.env["DATABASE_HOST"],
-    user: process.env["DATABASE_USER"],
-    password: process.env["DATABASE_PASSWORD"],
-    database: process.env["DATABASE_NAME"],
-  });
-
-  const conn = await db.getConnection();
-  try {
-    await conn.query("SELECT 1");
-    console.log("Connection successful...");
-  } finally {
-    await conn.end();
-  }
-}
 
 async function main() {
-  await connect();
-
-  app.get("/", (req, res) => {
-    console.log("Hello");
-    res.send("Hello!");
-  });
-
+  const app = await setupServer();
   app.listen(port, "0.0.0.0", () => {
     console.info(`App listening on ${port}.`);
   });
 }
 
-console.log("~~~~~\n\n\nSERVER\n\n\n~~~~~")
 await main();
