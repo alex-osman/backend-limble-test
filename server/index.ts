378060e2a9cf85e024b9dad6259cec4636f17249
Alex Osman <alexosman@Alexs-MBP.fios-router.home>
2024-09-23 12:54:53 -0400

    init commit typescript

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import * as mariadb from "mariadb";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 const port = 3000;
 let db;
 
@@ -11,12 +13,13 @@ async function connect() {
     host: process.env["DATABASE_HOST"],
     user: process.env["DATABASE_USER"],
     password: process.env["DATABASE_PASSWORD"],
-    database: process.env["DATABASE_NAME"]
+    database: process.env["DATABASE_NAME"],
   });
 
   const conn = await db.getConnection();
   try {
     await conn.query("SELECT 1");
+    console.log("Connection successful...");
   } finally {
     await conn.end();
   }
@@ -26,6 +29,7 @@ async function main() {
   await connect();
 
   app.get("/", (req, res) => {
+    console.log("Hello");
     res.send("Hello!");
   });
 
@@ -34,4 +38,5 @@ async function main() {
   });
 }
 
+console.log("~~~~~\n\n\nSERVER\n\n\n~~~~~")
 await main();
